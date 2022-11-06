568b026bf8f06c6cca2d8f38ebeafa3613ed8fab
Alex Pushkin <apushkaa@gmail.com>
2022-11-06 20:38:56 +0300

    rrd v5 initial

--- src/App.js
+++ src/App.js
@@ -1,24 +1,100 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import { Link, Redirect, Route, Switch, useParams } from "react-router-dom";
+
+const UsersLayout = () => {
+  const { userId } = useParams();
+
+  return (
+    <div>
+      <h1>Users Layout</h1>
+      <Link to="/">Main Page</Link>
+
+      {userId ? (
+        <Switch>
+          <Route path={`/users/:userId/profile`} component={UserInfoPage} />
+          <Route path={`/users/:userId/edit`} component={EditUserPage} />
+          <Route path="/users/*">
+            <Redirect to={`/users/${userId}/profile`} />
+          </Route>
+        </Switch>
+      ) : (
+        <UserListPage />
+      )}
+    </div>
+  );
+};
+
+const UserListPage = () => {
+  const [users] = useState([1, 2, 3, 4, 5]);
+  return (
+    <>
+      <h1>Users List Page</h1>
+      <ul>
+        {users.map((user, index) => (
+          <li key={index}>
+            <Link to={`/users/${user}`}>User {user}</Link>
+          </li>
+        ))}
+      </ul>
+    </>
+  );
+};
+
+const UserInfoPage = () => {
+  const { userId } = useParams();
+  return (
+    <>
+      <h1>User Page</h1>
+      <ul>
+        <li>
+          <Link to="/users">Users List Page</Link>
+        </li>
+        <li>
+          <Link to={`/users/${userId}/edit`}>Edit this user</Link>
+        </li>
+      </ul>
+      <p>userId: {userId}</p>
+    </>
+  );
+};
+
+const EditUserPage = () => {
+  const { userId } = useParams();
+  return (
+    <>
+      <h1>Edit User Page</h1>
+      <ul>
+        <li>
+          <Link to={`/users/${userId}/profile`}>User Profile Page</Link>
+        </li>
+        <li>
+          <Link to={`/users/${Number(userId) + 1}/profile`}>Another User</Link>
+        </li>
+        <li>
+          <Link to={`/users`}>Users List Page</Link>
+        </li>
+      </ul>
+    </>
+  );
+};
+
+const MainPage = () => {
+  return <h1>Main Page</h1>;
+};
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <h1>App Layout</h1>
+      <Link to="/users">Users List Page</Link>
+      <Switch>
+        <Route path="/users/:userId?" component={UsersLayout} />
+        <Route exact path="/" component={MainPage} />
+        <Route path="*">
+          <Redirect to="/" />
+        </Route>
+      </Switch>
+    </>
   );
 }
 
